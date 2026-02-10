8ab509d2cdd623450e01a3f696c95576c2b0e46a
Laura Garcia <laura.garcia05051998@gmail.com>
2026-02-11 12:36:44 +1300

    Add global CSS, custom fonts and Image component

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Bricolage_Grotesque, Montserrat } from 'next/font/google'
+
+export const bricolage = Bricolage_Grotesque({ subsets: ['latin'] })
+
+export const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+})
