df9a751bd57bb5fb02385ffb5f34869e150ac47f
Laura Garcia <laura.garcia05051998@gmail.com>
2026-02-12 11:53:22 +1300

    Add lusitana font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,13 @@
-import { Bricolage_Grotesque, Montserrat } from 'next/font/google'
+import { Bricolage_Grotesque, Montserrat, Lusitana } from 'next/font/google'
 
 export const bricolage = Bricolage_Grotesque({ subsets: ['latin'] })
 
 export const montserrat = Montserrat({
   subsets: ['latin'],
   weight: ['400', '500', '600', '700'],
+})
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
 })
